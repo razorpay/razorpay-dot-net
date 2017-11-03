d62245b8f4279d2bb3cfa8578e9e06190b63dc81
mayankamencherla <mayank.amencherla@razorpay.com>
2017-11-03 16:38:02 +0530

    [v 2.1] Added VirtualAccount API's

--- src/VirtualAccount.cs
+++ src/VirtualAccount.cs
@@ -4,6 +4,27 @@ namespace Razorpay.Api
 {
     public class VirtualAccount : Entity
     {
-        
+        new public VirtualAccount Fetch(string id)
+        {
+            return (VirtualAccount)base.Fetch(id);
+        }
+
+        new public List<VirtualAccount> All(Dictionary<string, object> options = null)
+        {
+            List<Entity> entities = base.All(options);
+            List<VirtualAccount> virtualaccounts = new List<VirtualAccount>();
+            foreach (Entity entity in entities)
+            {
+                virtualaccounts.Add(entity as VirtualAccount);
+            }
+            return virtualaccounts;
+        }
+
+        public VirtualAccount Create(Dictionary<string, object> data)
+        {
+            string relativeUrl = GetEntityUrl();
+            List<Entity> entities = Request(relativeUrl, HttpMethod.Post, data);
+            return (VirtualAccount)entities[0];
+        }
     }
 }
