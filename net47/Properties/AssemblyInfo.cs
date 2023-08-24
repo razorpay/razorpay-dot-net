351f7951a14bc91a3b7d904efcc7d31e70b95494
ankitdas13 <ankit.das@razorpay.com>
2023-08-24 19:20:40 +0530

    comment fix

--- net47/Properties/AssemblyInfo.cs
+++ net47/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Razorpay Software Private Limited")]
 [assembly: AssemblyProduct("Razorpay.API")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
