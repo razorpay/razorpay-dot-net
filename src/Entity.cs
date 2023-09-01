4ccafa8bd6177598ce7b2b58b824da189db7d5f6
ankitdas13 <ankit.das@razorpay.com>
2023-09-01 18:10:51 +0530

    add entity file

--- src/Entity.cs
+++ src/Entity.cs
@@ -24,6 +24,7 @@ namespace Razorpay.Api
             {"subscription", "Razorpay.Api.Subscription"},
             {"virtual_account", "Razorpay.Api.VirtualAccount"},
             {"addon", "Razorpay.Api.Addon"},
+            {"stakeholder", "Razorpay.Api.Stakeholder"},
         };
         private static List<HttpMethod> JsonifyInput = new List<HttpMethod>()
         {
