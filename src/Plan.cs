cc3150b2749e44c2da9a09f9c156f705d144cd45
ankitdas13 <ankit.das@razorpay.com>
2023-09-11 17:32:24 +0530

    added plan entity

--- src/Plan.cs
+++ src/Plan.cs
@@ -22,7 +22,7 @@ namespace Razorpay.Api
 
         public Plan Create(Dictionary<string, object> data = null)
         {
-            string relativeUrl = GetEntityUrl();
+            string relativeUrl = string.Format("{0}/{1}", GetUrlVersion(), GetEntityUrl());
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, data);
             return (Plan)entities[0];
         }
