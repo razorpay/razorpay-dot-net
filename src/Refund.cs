2982726cb3ff463d3a129f71a82309cf7c6e21ea
ankitdas13 <ankit.das@razorpay.com>
2023-09-11 17:43:35 +0530

    update refund entity

--- src/Refund.cs
+++ src/Refund.cs
@@ -11,14 +11,15 @@ namespace Razorpay.Api
 
         public Refund Create(Dictionary<string, object> data = null)
         {
-            string relativeUrl = GetEntityUrl();
+            string relativeUrl = string.Format("{0}/{1}/", GetUrlVersion(), GetEntityUrl());
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, data);
             return (Refund)entities[0];
         }
 
         new public List<Refund> All(Dictionary<string, object> options = null)
         {
-            List<Entity> entities = Request(GetEntityUrl(), HttpMethod.GET, options);
+            string relativeUrl = string.Format("{0}/{1}/", GetUrlVersion(), GetEntityUrl());
+            List<Entity> entities = Request(relativeUrl, HttpMethod.GET, options);
             List<Refund> refunds = new List<Refund>();
             foreach (Entity entity in entities)
             {
@@ -27,5 +28,11 @@ namespace Razorpay.Api
             return refunds;
         }
 
+        public Refund Edit(Dictionary<string, object> data)
+        {
+            string relativeUrl = string.Format("{0}/{1}/{2}", GetUrlVersion(), GetEntityUrl(), this["id"]);
+            List<Entity> entities = Request(relativeUrl, HttpMethod.PATCH, data);
+            return (Refund)entities[0];
+        }
     }
 }
