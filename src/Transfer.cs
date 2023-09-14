dfe506e6567ff10e4abde4690494370f6fb53fc3
ankitdas13 <ankit.das@razorpay.com>
2023-09-14 19:32:35 +0530

    update transfer

--- src/Transfer.cs
+++ src/Transfer.cs
@@ -16,14 +16,14 @@ namespace Razorpay.Api
 
         public Transfer Edit(Dictionary<string, object> options = null)
         {
-            string relativeUrl = string.Format("{0}/{1}", GetEntityUrl(), this["id"]);
-            List<Entity> entities = Request(relativeUrl, HttpMethod.PATCH, options);   
+            string relativeUrl = string.Format("{0}/{1}/{2}", GetUrlVersion(), GetEntityUrl(), this["id"]);
+            List<Entity> entities = Request(relativeUrl, HttpMethod.PATCH, options);
             return (Transfer)entities[0];
         }
 
         public Transfer Create(Dictionary<string, object> options = null)
         {
-            string relativeUrl = GetEntityUrl();
+            string relativeUrl = string.Format("{0}/{1}", GetUrlVersion(), GetEntityUrl());
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, options);
             return (Transfer)entities[0];
         }
@@ -33,14 +33,14 @@ namespace Razorpay.Api
         **/
         public Reversal Reversal(Dictionary<string, object> options = null)
         {
-            string relativeUrl = string.Format("{0}/{1}/reversals", GetEntityUrl(), this["id"]);
+            string relativeUrl = string.Format("{0}/{1}/{2}/reversals", GetUrlVersion(), GetEntityUrl(), this["id"]);
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, options);
             return (Reversal)entities[0];
         }
 
         public List<Reversal> Reversals(Dictionary<string, object> options = null)
         {
-            string relativeUrl = string.Format("{0}/{1}/reversals", GetEntityUrl(), this["id"]);
+            string relativeUrl = string.Format("{0}/{1}/{2}/reversals", GetUrlVersion(), GetEntityUrl(), this["id"]);
             List<Entity> entities = Request(relativeUrl, HttpMethod.GET, options);
             List<Reversal> reversals = new List<Reversal>();
 
@@ -50,6 +50,17 @@ namespace Razorpay.Api
             }
 
             return reversals;
+        }
+
+        public List<Transfer> All(Dictionary<string, object> options = null)
+        {
+            List<Entity> entities = base.All(options);
+            List<Transfer> transfers = new List<Transfer>();
+            foreach (Entity entity in entities)
+            {
+                transfers.Add(entity as Transfer);
+            }
+            return transfers;
         }
     }
 }
