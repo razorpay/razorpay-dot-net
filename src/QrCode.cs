6d5a30cbaf3246fa3b163e1d08bc23ac2e1c0f17
ankitdas13 <ankit.das@razorpay.com>
2023-09-21 14:19:27 +0530

    baseurl changes

--- src/QrCode.cs
+++ src/QrCode.cs
@@ -5,23 +5,28 @@ namespace Razorpay.Api
 {
     public class QrCode : Entity
     {
+        public string QrCodeUrl(string qrcode_url= "payments/qr_codes")
+        {  
+            return qrcode_url;
+        }
+
         new public QrCode Fetch(string id)
         {
-            string relativeUrl = string.Format("{0}/payments/qr_codes/{1}", GetUrlVersion(), id);
+            string relativeUrl = string.Format("{0}/{1}/{2}", GetUrlVersion(), QrCodeUrl(), id);
             List<Entity> entities = Request(relativeUrl, HttpMethod.GET, null);
             return (QrCode)entities[0];
         }
 
         public QrCode Create(Dictionary<string, object> data)
         {
-            string relativeUrl = string.Format("{0}/payments/qr_codes", GetUrlVersion());
+            string relativeUrl = string.Format("{0}/{1}", GetUrlVersion(), QrCodeUrl());
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, data);
             return (QrCode)entities[0];
         }
 
         public List<QrCode> All(Dictionary<string, object> options = null)
         {
-            string relativeUrl = string.Format("{0}/payments/qr_codes", GetUrlVersion());
+            string relativeUrl = string.Format("{0}/{1}", GetUrlVersion(), QrCodeUrl());
             List<Entity> entities = Request(relativeUrl, HttpMethod.GET, options);
             List<QrCode> qrcodes = new List<QrCode>();
             foreach (Entity entity in entities)
@@ -33,7 +38,7 @@ namespace Razorpay.Api
 
         public List<Payment> FetchAllPayments(string id, Dictionary<string, object> options = null)
         {
-            string relativeUrl = string.Format("{0}/payments/qr_codes/{1}/payments", GetUrlVersion(), id);
+            string relativeUrl = string.Format("{0}/{1}/{2}/payments", GetUrlVersion(), QrCodeUrl(), id);
             List<Entity> entities = Request(relativeUrl, HttpMethod.GET, options);
             List<Payment> payments = new List<Payment>();
             foreach (Entity entity in entities)
@@ -45,7 +50,7 @@ namespace Razorpay.Api
 
         public QrCode Close()
         {
-            string relativeUrl = string.Format("{0}/payments/qr_codes/{1}/close", GetUrlVersion(), this["id"]);
+            string relativeUrl = string.Format("{0}/{1}/{2}/close", GetUrlVersion(), QrCodeUrl(), this["id"]);
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, null);
             return (QrCode)entities[0];
         }
