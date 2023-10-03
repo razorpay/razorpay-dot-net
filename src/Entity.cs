1398bd9e6be024672e850edc89fa69fafd87d46b
Ankit Das <89454448+ankitdas13@users.noreply.github.com>
2023-10-03 15:28:37 +0530

    added tnc entity (#92)

--- src/Entity.cs
+++ src/Entity.cs
@@ -24,6 +24,7 @@ namespace Razorpay.Api
             {"subscription", "Razorpay.Api.Subscription"},
             {"virtual_account", "Razorpay.Api.VirtualAccount"},
             {"addon", "Razorpay.Api.Addon"},
+            {"tnc_map", "Razorpay.Api.Tnc"},
             {"item", "Razorpay.Api.Item" },
             {"account", "Razorpay.Api.Account"},
             {"stakeholder", "Razorpay.Api.Stakeholder"},
--- src/RazorpayClient.cs
+++ src/RazorpayClient.cs
@@ -24,6 +24,7 @@ namespace Razorpay.Api
         private Plan plan = null;
         private Subscription subscription = null;
         private VirtualAccount virtualaccount = null;
+        private Tnc tnc = null;
         private Item item = null;
         private Account account = null;
         private Stakeholder stakeholder = null;
@@ -250,6 +251,18 @@ namespace Razorpay.Api
                     virtualaccount = new VirtualAccount();
                 }
                 return virtualaccount;
+            }
+        }
+
+        public Tnc Tnc
+        {
+            get
+            {
+                if (tnc == null)
+                {
+                    tnc = new Tnc();
+                }
+                return tnc;
             }
         }
 
