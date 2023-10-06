8b3142e37011c456c3c95b147062894af17bb51b
ankitdas13 <ankit.das@razorpay.com>
2023-10-06 14:42:12 +0530

    add check

--- src/Invoice.cs
+++ src/Invoice.cs
@@ -31,35 +31,70 @@ namespace Razorpay.Api
         {
             string relativeUrl = string.Format("{0}/{1}/{2}", GetUrlVersion(), GetEntityUrl(), this["id"]);
             List<Entity> entities = Request(relativeUrl, HttpMethod.PATCH, data);
-            return (Invoice)entities[0];
+            if (entities != null && entities.Count > 0)
+            {
+                return (Invoice)entities[0];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public Invoice Cancel()
         {
             string relativeUrl = string.Format("{0}/invoices/{1}/cancel", GetUrlVersion(), this["id"]);
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, null);
-            return (Invoice)entities[0];
+            if (entities != null && entities.Count > 0)
+            {
+                return (Invoice)entities[0];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public Invoice NotifyBy(string medium)
         {
             string relativeUrl = string.Format("{0}/invoices/{1}/notify_by/{2}", GetUrlVersion(), this["id"], medium);
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, null);
-            return (Invoice)entities[0];
+            if (entities != null && entities.Count > 0)
+            {
+                return (Invoice)entities[0];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public Invoice CreateRegistrationLink(Dictionary<string, object> data)
         {
             string relativeUrl = string.Format("{0}/subscription_registration/auth_links", GetUrlVersion());
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, data);
-            return (Invoice)entities[0];
+            if (entities != null && entities.Count > 0)
+            {
+                return (Invoice)entities[0];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public Invoice Issue()
         {
             string relativeUrl = string.Format("{0}/invoices/{1}/issue", GetUrlVersion(), this["id"]);
             List<Entity> entities = Request(relativeUrl, HttpMethod.POST, null);
-            return (Invoice)entities[0];
+            if (entities != null && entities.Count > 0)
+            {
+                return (Invoice)entities[0];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public List<Invoice> Delete()
