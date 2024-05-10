e2eabf07e7a91ef22451cddc06b1185848b61b2b
ankitdas13 <ankit.das@razorpay.com>
2024-05-10 14:40:24 +0530

    added all

--- src/Iin.cs
+++ src/Iin.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace Razorpay.Api
@@ -12,5 +13,11 @@ namespace Razorpay.Api
             return (Iin)entities[0];
         }
 
+        public Iin All(Dictionary<string, object> data)
+        {
+            string relativeUrl = string.Format("{0}/{1}/list", GetUrlVersion(), GetEntityUrl());
+            List<Entity> entities = Request(relativeUrl, HttpMethod.GET, data);
+            return (Iin)entities[0];
+        }
     }
 }
