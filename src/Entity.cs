razorpay/razorpay-dot-net

commit a8f16aedfa86725bade5527a0cf6c741926032c3

feat: Introduce AuthType and refactor authentication

src/Entity.cs
@@ -43,7 +43,7 @@ namespace Razorpay.Api
             {"methods","Razorpay.Api.Method"},
             {"dispute", "Razorpay.Api.Dispute"},
             {"bank_account", "Razorpay.Api.BankAccount"},
-            {"device.activity", "Razorpay.Api.DeviceActivity"},
+            {"devices.activity", "Razorpay.Api.DeviceActivity"},
         };
       
         private static List<HttpMethod> JsonifyInput = new List<HttpMethod>()
@@ -79,7 +79,7 @@ namespace Razorpay.Api
             return Request(relativeUrl, verb, options, "API");
         }
 
-        protected List<Entity> Request(string relativeUrl, HttpMethod verb, Dictionary<string, object> options, string host)
+        protected List<Entity> Request(string relativeUrl, HttpMethod verb, Dictionary<string, object> options, string host, AuthType authType = AuthType.Private)
         {
             client = new RestClient();
             string postData = string.Empty;
@@ -95,7 +95,7 @@ namespace Razorpay.Api
                 postData = JsonConvert.SerializeObject(options);
             }
 
-            string responseStr = client.MakeRequest(relativeUrl, verb, postData, host);
+            string responseStr = client.MakeRequest(relativeUrl, verb, postData, host, authType);
 
             dynamic response = JsonConvert.DeserializeObject(responseStr);
 

.gitignore
@@ -1,16 +1,6 @@
-*.DS_Store
-*.userprefs
-*.suo
-*.user
-.vs
-bin
-packages
-TestResults
-/net40/obj
-/net45/obj
-/net40/bin
-/net45/bin
-/test/net40/obj
-/test/net40/bin
-/test/net45/obj
-/test/net45/bin
+# IDE and build output
+.idea/
+net48/obj/
+net60/obj/
+bin/
+obj/

Razorpay.csproj
@@ -1,52 +1,0 @@
-<Project Sdk="Microsoft.NET.Sdk">
-
-  <PropertyGroup>
-    <TargetFrameworks>net8.0;net6.0;netstandard2.0;net48</TargetFrameworks>
-    <AssemblyName>Razorpay</AssemblyName>
-    <RootNamespace>Razorpay.Api</RootNamespace>
-    <PackageId>Razorpay</PackageId>
-    <PackageVersion>3.1.4</PackageVersion>
-    <Authors>Razorpay</Authors>
-    <Company>Razorpay</Company>
-    <Product>Razorpay .NET SDK</Product>
-    <Description>Razorpay .NET SDK with POS Gateway Integration</Description>
-    <Copyright>Copyright © Razorpay 2024</Copyright>
-    <PackageLicenseExpression>MIT</PackageLicenseExpression>
-    <PackageProjectUrl>https://github.com/razorpay/razorpay-dotnet</PackageProjectUrl>
-    <RepositoryUrl>https://github.com/razorpay/razorpay-dotnet</RepositoryUrl>
-    <RepositoryType>git</RepositoryType>
-    <GeneratePackageOnBuild>false</GeneratePackageOnBuild>
-    <LangVersion>latest</LangVersion>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <PackageReference Include="Newtonsoft.Json" Version="13.0.3" />
-    <PackageReference Include="BouncyCastle.Cryptography" Version="2.2.1" />
-    <PackageReference Include="Microsoft.CSharp" Version="4.7.0" />
-  </ItemGroup>
-
-  <PropertyGroup>
-    <EnableDefaultCompileItems>false</EnableDefaultCompileItems>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <Compile Include="src\**\*.cs" />
-  </ItemGroup>
-
-  <ItemGroup>
-    <None Include="README.md" Pack="true" PackagePath="\" />
-    <None Include="Razorpay.nuspec" />
-  </ItemGroup>
-
-  <!-- .NET Framework specific references -->
-  <ItemGroup Condition="'$(TargetFramework)' == 'net48'">
-    <Reference Include="System" />
-    <Reference Include="System.Core" />
-    <Reference Include="System.Net" />
-    <Reference Include="System.Net.Http" />
-    <Reference Include="System.Web" />
-    <Reference Include="System.Xml" />
-    <Reference Include="System.Xml.Linq" />
-  </ItemGroup>
-
-</Project>

Razorpay.nuspec
@@ -2,7 +2,7 @@
 <package >
   <metadata>
     <id>Razorpay</id>
-    <version>3.1.4</version>
+    <version>3.2.0</version>
     <authors>razorpay</authors>
     <owners>razorpay</owners>
     <licenseUrl>https://github.com/razorpay/razorpay-dot-net/blob/master/licence.txt</licenseUrl>
@@ -23,5 +23,6 @@
     <file src="net45/bin/Release/Razorpay.dll" target="lib/net45/Razorpay.dll" />
     <file src="net47/bin/Release/Razorpay.dll" target="lib/net47/Razorpay.dll" />
     <file src="net48/bin/Release/Razorpay.dll" target="lib/net48/Razorpay.dll" />
+    <file src="net60/bin/Release/net6.0/Razorpay.dll" target="lib/net6.0/Razorpay.dll" />
   </files>
 </package>

net60/RazorpayClient60.csproj
@@ -4,7 +4,7 @@
     <OutputType>Library</OutputType>
     <RootNamespace>Razorpay.Api</RootNamespace>
     <AssemblyName>Razorpay</AssemblyName>
-    <Version>3.1.4</Version>
+    <Version>3.2.0</Version>
     <Authors>Razorpay</Authors>
     <Company>Razorpay</Company>
     <Product>Razorpay .NET SDK</Product>

src/AuthType.cs
@@ -1,0 +1,8 @@
+namespace Razorpay.Api
+{
+    public enum AuthType
+    {
+        Public,
+        Private
+    }
+} 

src/DeviceActivity.cs
@@ -18,7 +18,7 @@ namespace Razorpay.Api
         public DeviceActivity Create(Dictionary<string, object> data)
         {
             string relativeUrl = string.Format("{0}/{1}", GetUrlVersion(), GetEntityUrl());
-            List<Entity> entities = Request(relativeUrl, HttpMethod.POST, data, "POS");
+            List<Entity> entities = Request(relativeUrl, HttpMethod.POST, data, "API", AuthType.Public);
             return (DeviceActivity)entities[0];
         }
 
@@ -29,15 +29,10 @@ namespace Razorpay.Api
         /// <returns>DeviceActivity object with current status</returns>
         public DeviceActivity GetStatus(string activityId)
         {
-            if (string.IsNullOrWhiteSpace(activityId))
-            {
-                throw new System.ArgumentException("Activity ID cannot be null or empty", "activityId");
-            }
-
-            // Use the correct endpoint format: device/activity/{activity_id}
+            // Use the correct endpoint format: devices/activity/{activity_id}
             // Build the relative URL using the same entity path helper for consistency
             string relativeUrl = string.Format("{0}/{1}/{2}", GetUrlVersion(), GetEntityUrl(), activityId);
-            List<Entity> entities = Request(relativeUrl, HttpMethod.GET, null, "POS");
+            List<Entity> entities = Request(relativeUrl, HttpMethod.GET, null, "API", AuthType.Public);
             return (DeviceActivity)entities[0];
         }
 
@@ -48,7 +43,7 @@ namespace Razorpay.Api
         /// <returns>Entity URL for device activities</returns>
         protected new string GetEntityUrl()
         {
-            return "device/activity";
+            return "devices/activity";
         }
     }
 } 

src/HttpMethod.cs
@@ -10,4 +10,10 @@ namespace Razorpay.Api
         PUT,
         PATCH,
     }
+
+    public enum AuthType
+    {
+        Public,
+        Private
+    }
 }

commit 9ec7069716068416f9b12622838a6c894c692cef

fix: correct DeviceActivity entity mapping from 'devices.activity' to 'device.activity'
- Fix entity mapping to match actual API response format
- API returns 'device.activity' (singular) not 'devices.activity' (plural)
- This resolves casting issues where DeviceActivity API responses were falling back to generic Entity objects
- Restores functionality that was accidentally broken in commit a8f16ae

Fixes type casting errors when using client.DeviceActivity.Create() and client.DeviceActivity.GetStatus()

src/Entity.cs
@@ -43,7 +43,7 @@ namespace Razorpay.Api
             {"methods","Razorpay.Api.Method"},
             {"dispute", "Razorpay.Api.Dispute"},
             {"bank_account", "Razorpay.Api.BankAccount"},
-            {"devices.activity", "Razorpay.Api.DeviceActivity"},
+            {"device.activity", "Razorpay.Api.DeviceActivity"},
         };
       
         private static List<HttpMethod> JsonifyInput = new List<HttpMethod>()